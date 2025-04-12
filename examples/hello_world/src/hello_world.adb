24020376d2813fc6a20efdc4a735aa6dee30e671
Maxim Reznik <reznikmm@gmail.com>
2025-04-12 11:34:42 +0300

    Add stub code

---
+++ examples/hello_world/src/hello_world.adb
@@ -0,0 +1,9 @@
+--  SPDX-FileCopyrightText: 2025 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+procedure Hello_World is
+begin
+   null;
+end Hello_World;
---
+++ src/slides_as_code.ads
@@ -0,0 +1,8 @@
+--  SPDX-FileCopyrightText: 2025 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+package Slides_As_Code is
+
+end Slides_As_Code;
